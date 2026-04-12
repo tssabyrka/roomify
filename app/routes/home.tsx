5bc2be62d536011ebe1b5ce13c014eefdaf002a6
Sabyrka <tssabyrka@gmail.com>
2026-04-12 23:30:24 +0500

    chore: add Navbar to Home component and update layout structure

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,18 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
+import Navbar from '../../components/Navbar'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="home">
+      <Navbar />
+      <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+    </div>
+  )
 }
