eade335b1c8cd895c5004f8ddca25f1938c8d2ca
Sabyrka <tssabyrka@gmail.com>
2026-04-13 16:15:41 +0500

    feat: enhance upload component with drag-and-drop and progress state

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,6 +1,63 @@
+import { useState } from 'react'
+import { useOutletContext } from 'react-router'
+import { CheckCircle2, ImageIcon, UploadIcon } from 'lucide-react'
+
 const Upload = () => {
+  const [file, setFile] = useState<File | null>(null)
+  const [isDragging, setIsDragging] = useState(false)
+  const [progress, setProgress] = useState(0)
+
+  const { isSignedIn } = useOutletContext<AuthContext>()
+
   return (
-    <div>Upload</div>
+    <div className="upload">
+      {!file ? (
+        <div className={`dropzone ${isDragging ? 'is-dragging' : ''}`}>
+          <input
+            className="drop-input"
+            type="file"
+            accept=".jpg, .jpeg, .png"
+            disabled={!isSignedIn}
+          />
+
+          <div className="drop-content">
+            <div className="drop-icon">
+              <UploadIcon size={20} />
+            </div>
+            <p>
+              {isSignedIn ? (
+                'Click to upload or just drag and drop'
+              ) : (
+                'Sign in or sign up with Puter to upload'
+              )}
+            </p>
+            <p className="help">Maximum file size 50MB.</p>
+          </div>
+        </div>
+      ) : (
+        <div className="upload-status">
+          <div className="status-content">
+            <div className="status-icon">
+              {progress === 100 ? (
+                <CheckCircle2 className="check" />
+              ) : (
+                <ImageIcon className="image" />
+              )}
+            </div>
+
+            <h3>{file.name}</h3>
+
+            <div className="progress">
+              <div className="bar" style={{ width: `${progress}%` }} />
+
+              <p className="status-text">
+                {progress < 100 ? 'Analyzing Floor Plan...' : 'Redirecting...'}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
