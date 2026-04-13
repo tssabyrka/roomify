5a99ce96655f4fafc9ccd8ca693b341221fbfa26
Sabyrka <tssabyrka@gmail.com>
2026-04-13 16:03:32 +0500

    feat: add Upload component and integrate into home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import { ArrowRight, ArrowUpRight, Clock, Layers } from 'lucide-react'
 import type { Route } from './+types/home'
 import Navbar from '../../components/Navbar'
 import Button from '../../components/ui/Button'
+import Upload from '../../components/Upload'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -53,7 +54,7 @@ export default function Home() {
               <p>Supports JPG, PNG, formats up to 10MB</p>
             </div>
 
-            <p>Upload images</p>
+            <Upload />
           </div>
         </div>
       </section>
